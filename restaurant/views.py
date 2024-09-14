5dea031a62015e204ffa2b20a51c2cf9496a3142
jichimura1973 <jichimura1973@gmail.com>
2024-09-14 10:59:45 +0900

    6.トップページの実装までを実施（動作未確認）

--- restaurant/views.py
+++ restaurant/views.py
@@ -12,6 +12,28 @@ class TopPageView(generic.ListView):
     guery_set = models.Restaurant.objects.order_by('-rate')
     context_object_name = 'retaurant_list'
     
+    def get_context_data(self, **kwargs):
+        if 'price_session' in self.request.session:
+            self.request.session['price_session'] = 0
+        
+        if 'keyword_session' in self.request.session:
+            self.request.session['keyword_session'] = ''
+        
+        if 'category_session' in self.request.session:
+            self.request.session['category_session'] = ''
+        
+        if 'select_sort' in self.request.session:
+            self.request.session['select_sort'] = '-created_at'
+        
+        context = super(TopPageView, self).get_context_data(**kwargs)
+        category_list = models.Category.objects.all()
+        new_restaurant_list = models.Restaurant.objects.all().order_by('-created_at')
+        context.update({
+            'category_list': category_list,
+            'new_restaurant_list': new_restaurant_list,
+        })
+        return context
+    
 """ 会社概要 ====================================="""
 
 class CompanyView(generic.TemplateView):
